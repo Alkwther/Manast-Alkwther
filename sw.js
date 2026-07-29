ec2b72d186f13adef6452f946b1b473406a72e0d
Alkwther <alkwther700@gmail.com>
2026-07-29 22:25:57 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kawthar-platform-v80';
+const CACHE_NAME = 'kawthar-platform-v81';
 const APP_SHELL = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
